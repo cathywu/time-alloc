Refactor for 6M (max) to also used refactored implementation; small optimization to apply fewer constraints on max chunking; documentation; fixed a sign error with the max chunk filters

scripts/calendar.py
@@ -10,10 +10,8 @@ task_duration = 6*np.ones(num_tasks)
 
 utilities = np.ones((num_tasks, num_timeslots)).T
 utilities[0, 0] = 0  # forces entry (0,0) to be unassigned
-task_chunk_min = 3*np.ones(num_tasks)
+task_chunk_min = 2*np.ones(num_tasks)
 task_chunk_min[0] = 1
-task_chunk_min[3] = 6
-task_chunk_min[4] = 6
 task_chunk_max = 5*np.ones(num_tasks)
 task_chunk_max[3] = 6
 

timealloc/calendar_solver.py
@@ -20,7 +20,7 @@ class CalendarSolver:
     def __init__(self, utilities, params):
         self.model = AbstractModel()
 
-        self.slack_cont = 1
+        self.slack_cont = 5
 
         # read parameters
         # self.num_tasks = Param(initialize=params['num_tasks'], default=5)
@@ -273,9 +273,10 @@ class CalendarSolver:
         :return:
         """
         filter = np.ones(chunk_len+offset*2)
-        filter[0:offset] = -1
-        filter[-offset:] = -1
-        print('XXX', chunk_len, mode, filter)
+        if offset > 0:
+            filter[0:offset] = -1
+            filter[-offset:] = -1
+        print('XXX Chunk filter:', chunk_len, mode, filter)
         # filter = np.array([-1, 1, 1, 1, 1, 1, 1, -1])
         L, b = util.linop_from_1d_filter(filter, self.num_timeslots,
                                          offset=offset)
@@ -300,14 +301,17 @@ class CalendarSolver:
             """
             C = operator.attrgetter(var_name)(model)[i, j]
             if mode == 'min':
+                # For min mode, need to check that none of the smaller chunks
+                # match (hence inequality)
                 if model.task_chunk_min[j] <= chunk_len:
                     return Constraint.Feasible
                 return None, C, chunk_len - 1
                 # return None, model.C6m[i, j], chunk_len - 1
             elif mode == 'max':
-                if model.task_chunk_max[j] >= chunk_len:
-                    return Constraint.Feasible
-                return None, C, filter.size - 1
+                # For max mode, only need to check once (hence equality)
+                if model.task_chunk_max[j]+1 == chunk_len:
+                    return None, C, filter.size - 1
+                return Constraint.Feasible
 
         return rule
 
@@ -326,11 +330,13 @@ class CalendarSolver:
         def rule(model, i, j):
             """
             Lower bounds on filter match
+
+            See CalendarSolver._get_rule_chunk_upper() for more details.
             """
             C = operator.attrgetter(var_name)(model)[i, j]
             if mode == 'min' and model.task_chunk_min[j] <= chunk_len:
                 return Constraint.Feasible
-            elif mode == 'max' and model.task_chunk_max[j] >= chunk_len:
+            elif mode == 'max' and model.task_chunk_max[j]+1 != chunk_len:
                 return Constraint.Feasible
             total = sum(L[i, k] * model.A[k, j] for k in model.timeslots)
             return 0, C - total, None
@@ -339,7 +345,7 @@ class CalendarSolver:
 
     def _constraints_chunking6m(self):
         """
-        Ensures there are no tasks allocated below a minimum chunk length of 6
+        Ensures there are no unwanted 6-chunk tasks
         """
         chunk_len = 6
         mode = 'min'
@@ -362,40 +368,28 @@ class CalendarSolver:
 
     def _constraints_chunking6M(self):
         """
-        Ensures there are no tasks allocated beyond a maximum chunk length of 6
+        Ensures there are no unwanted 6+ chunk tasks
 
         FIXME(cathywu) this seems to slow down solving significantly.
         """
-        chunk_len = 7
-        offset = 0
-        filter = np.ones(chunk_len)
-        L, b = util.linop_from_1d_filter(filter, self.num_timeslots,
-                                         offset=offset)
-        c_len = self.num_timeslots - filter.size + 1 + offset * 2
+        chunk_len = 6
+        mode = 'max'
+        var_name = 'C6M'
+        offset = 1 if mode == 'min' else 0
+
+        filter, L, c_len = self._get_chunk_parameters(chunk_len, offset, mode)
 
         self.model.c6Mtimeslots = RangeSet(0, c_len - 1)
         self.model.C6M = Var(self.model.c6Mtimeslots * self.model.tasks,
                              domain=pe.Reals)
 
-        def rule(model, i, j):
-            """
-            Disallow any chunks of length of the above filter
-            """
-            if model.task_chunk_max[j] >= chunk_len:
-                return Constraint.Feasible
-            return None, model.C6M[i, j], filter.size - 1
-
+        rule = self._get_rule_chunk_upper(mode, var_name, chunk_len, filter)
         self.model.constrain_chunk6M0 = Constraint(self.model.c6Mtimeslots,
-                                                  self.model.tasks, rule=rule)
+                                                   self.model.tasks, rule=rule)
 
-        def rule(model, i, j):
-            if model.task_chunk_max[j] >= chunk_len:
-                return Constraint.Feasible
-            total = sum(L[i, k] * model.A[k, j] for k in model.timeslots)
-            return 0, model.C6M[i, j] - total, None
-
+        rule = self._get_rule_chunk_lower(mode, var_name, chunk_len, L)
         self.model.constrain_chunk6M1 = Constraint(self.model.c6Mtimeslots,
-                                                  self.model.tasks, rule=rule)
+                                                   self.model.tasks, rule=rule)
 
     def _constraints_switching_bounds(self):
         """
@@ -452,7 +446,7 @@ class CalendarSolver:
         self._constraints_chunking1()
         self._constraints_chunking2()
         self._constraints_task_contiguity()  # FIXME(cathywu) some slowdown
-        # self._constraints_chunking6M()  # FIXME(cathywu) dramatic slowdown
+        self._constraints_chunking6M()  # FIXME(cathywu) dramatic slowdown
         self._constraints_chunking6m()
         # objective
         self._objective_cost()